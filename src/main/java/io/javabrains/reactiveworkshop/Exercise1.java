fa9fbb7b44a3b00b6e142bd66eda9ab29d7e1a0b
Rahul Chattopadhyay <rahul.yay@gmail.com>
2025-01-12 23:12:05 +0530

    Exercise 1 and 2

--- src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise1.java
@@ -8,23 +8,56 @@ public class Exercise1 {
 
         // Print all numbers in the intNumbersStream stream
         // TODO: Write code here
+        System.out.println("Print all numbers in the intNumbersStream stream");
+        StreamSources.intNumbersStream().forEach(System.out::println);
 
         // Print numbers from intNumbersStream that are less than 5
         // TODO: Write code here
+        System.out.println("Print numbers from intNumbersStream that are less than 5");
+        StreamSources.intNumbersStream().filter(val -> val < 5)
+                .forEach(System.out::println);
 
         // Print the second and third numbers in intNumbersStream that's greater than 5
         // TODO: Write code here
+        System.out.println("Print the second and third numbers in intNumbersStream that's greater than 5");
+        StreamSources.intNumbersStream().filter(val -> val > 5)
+                .skip(1)
+                .limit(2)
+                .forEach(System.out::println);
 
         //  Print the first number in intNumbersStream that's greater than 5.
         //  If nothing is found, print -1
         // TODO: Write code here
+        System.out.println("Print the first number in intNumbersStream that's greater than 18. If nothing is found, print -1");
+        StreamSources.intNumbersStream().filter(val -> val > 18)
+                .findFirst()
+                .ifPresentOrElse(System.out::println, () -> System.out.println(-1));
+
+        System.out.println("Print the first number in intNumbersStream that's greater than 5. If nothing is found, print -1");
+        System.out.println(StreamSources.intNumbersStream().filter(val -> val > 5)
+                .findFirst()
+                .orElse(-1));
 
         // Print first names of all users in userStream
         // TODO: Write code here
+        System.out.println("Print first names of all users in userStream");
+        StreamSources.userStream()
+                .map(user -> user.getFirstName())
+                .forEach(System.out::println);
 
         // Print first names in userStream for users that have IDs from number stream
         // TODO: Write code here
+        System.out.println("Print first names in userStream for users that have IDs from number stream");
+        StreamSources.intNumbersStream()
+                .flatMap(id -> StreamSources.userStream().filter(user -> id == user.getId()))
+                .map(user -> user.getFirstName())
+                .forEach(System.out::println);
 
+        System.out.println("Print first names in userStream for users that have IDs from number stream");
+        StreamSources.userStream()
+                .filter(user -> StreamSources.intNumbersStream().anyMatch(id -> user.getId() == id))
+                .map(user -> user.getFirstName())
+                .forEach(System.out::println);
     }
 
 }
--- src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
@@ -10,9 +10,15 @@ public class Exercise2 {
 
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
         // TODO: Write code here
+        System.out.println("Print all numbers in the ReactiveSources.intNumbersFlux stream");
+        // ReactiveSources.intNumbersFlux().toIterable().forEach(System.out::println);
+        ReactiveSources.intNumbersFlux().subscribe(System.out::println);
 
         // Print all users in the ReactiveSources.userFlux stream
         // TODO: Write code here
+        System.out.println("Print all users in the ReactiveSources.userFlux stream");
+        //ReactiveSources.userFlux().toIterable().forEach(System.out::println);
+        ReactiveSources.userFlux().subscribe(System.out::println);
 
         System.out.println("Press a key to end");
         System.in.read();
